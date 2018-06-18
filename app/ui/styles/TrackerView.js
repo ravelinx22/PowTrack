35cfe4aad118c25b1fc4c44ea1361757fd9f42b3
William Ravelo <drummerwilliam@gmail.com>
2018-06-17 22:07:12 -0500

    added btn save

--- app/ui/styles/TrackerView.js
+++ app/ui/styles/TrackerView.js
@@ -1,14 +1,30 @@
 import { StyleSheet } from 'react-native';
 
 /* Tracker View */
-export default StyleSheet.create({
-	container: {
-		padding: 30,
-	},
-	circles_container: {
-		flex: 1,
-		flexDirection: 'column',
-		justifyContent: 'space-evenly',
-		alignItems: 'center',
+export function getStyles(save_btn_shown) {
+	if(save_btn_shown) {
+		return  StyleSheet.create({
+			circles_container: {
+				backgroundColor: "blue",
+				height: "90%",
+				flexDirection: 'column',
+			},
+			save_btn: {
+				height: "10%",
+				backgroundColor: "black"
+			}
+		});
+	} else {
+		return  StyleSheet.create({
+			circles_container: {
+				backgroundColor: "blue",
+				height: "100%",
+				flexDirection: 'column',
+			},
+			save_btn: {
+				height: "0%",
+				backgroundColor: "black"
+			}
+		});
 	}
-});
+}
--- app/ui/views/TrackerView.js
+++ app/ui/views/TrackerView.js
@@ -1,6 +1,6 @@
 import React, { Component  } from 'react';
-import { Text,View, Button } from 'react-native';
-import styles from "../styles/TrackerView";
+import { Text,View, Button, ScrollView } from 'react-native';
+import { getStyles } from "../styles/TrackerView";
 
 import TrackerRow from "../components/TrackerRow";
 
@@ -9,16 +9,32 @@ export default class TrackerView extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			constant: 5
+			constant: 5,
+			btnSaveShown: true,
 		};
+	}
+
+	_displayBtnSave() {
+		this.setState({
+			btnSaveShown: true,
+		});
+	}
+
+	_hideBtnSave() {
+		this.setState({
+			btnSaveShown: false,
+		});
 	}
 
 	render() {
 		return (
-			<View style={styles.circles_container}>
-				<TrackerRow constant={this.state.constant} />
-				<TrackerRow constant={this.state.constant} />
-				<TrackerRow constant={this.state.constant} />
+			<View>
+				<ScrollView contentContainerStyle={{flexGrow: 1}} style={getStyles(this.state.btnSaveShown).circles_container} >
+					<TrackerRow constant={this.state.constant}/>
+					<TrackerRow constant={this.state.constant}/>
+					<TrackerRow constant={this.state.constant}/>
+				</ScrollView>
+				<Text style={getStyles(this.state.btnSaveShown).save_btn}>algo</Text>
 			</View>
 		);
 	}
