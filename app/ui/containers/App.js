05ea4ca932d4d2961e9834ebe544c916337a87f6
William Ravelo <drummerwilliam@gmail.com>
2018-06-20 19:33:46 -0500

    new app structure

--- app/ui/containers/App.js
+++ app/ui/containers/App.js
@@ -9,7 +9,23 @@ import { createMaterialTopTabNavigator } from "react-navigation";
 import TrackerView from "../views/TrackerView";
 import { getFirst, getSecond } from "../utils/testData";
 
-export default createMaterialTopTabNavigator({
-	"Big 3": props => <TrackerView {...props} data={getFirst()}/>,
-	"Paused Big 3": props => <TrackerView {...props} data={getSecond()}/>,
-});
+
+export default class App extends Component<{}> {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+		}
+	}
+
+	render() {
+		const TabNavigator = createMaterialTopTabNavigator({
+			"Big 3": props => <TrackerView {...props} data={getFirst()}/>,
+			"Paused Big 3": props => <TrackerView {...props} data={getSecond()}/>,
+		});
+
+		return (
+			<TabNavigator/>
+		);
+	}
+}
